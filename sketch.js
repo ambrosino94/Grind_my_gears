2d62e8fa4930bff033a78da4f1d6a04d615875cd
Giacomo Echevers <ambrosino94@gmail.com>
2018-08-10 16:10:41 -0500

    A lil test

--- sketch.js
+++ sketch.js
@@ -3,5 +3,11 @@ function setup() {
 }
 
 function draw() {
+	const centerX = windowWidth/2;
+	const centerY = windowHeight/2;
+
+	background(255,170,120);
+	rect(centerX,centerY,mouseX-centerX,mouseY-centerY);
+	console.log(mouseY,mouseX);
 
 }
